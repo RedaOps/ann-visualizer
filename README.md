e4c816ed86adc52ee322e672c37a09658ba09d86
Prodicode <tudor@prodicode.com>
2018-03-27 21:07:03 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,58 @@
+![photo](https://i.imgur.com/DrZJOzy.png)
+![photo](https://i.imgur.com/9VfLaco.png)
+
+# ANN Visualizer
+A great visualization python library used to work with Keras. It uses python's graphviz library to create a presentable graph of the neural network you are building.
+
+## Installation
+1. Download the `ann_visualizer.py` file from the github repository.
+2. Place the `ann_visualizer.py` in the same directory as your main python script.
+3. Just import the function and use it:
+
+```python
+
+from ann_visualizer import visualize
+#Build your model here
+visualize(model)
+```
+
+## Documentation
+
+### visualize(model, view=True, filename="network.gv")
+* `model` - The Keras Sequential model
+* `view` - If True, it opens the graph preview after executed
+* `filename` - Where to save the graph. (.gv file format)
+
+## Example
+```python
+import keras;
+from keras.models import Sequential;
+from keras.layers import Dense;
+
+network = Sequential();
+        #Hidden Layer#1
+network.add(Dense(units=6,
+                  activation='relu',
+                  kernel_initializer='uniform',
+                  input_dim=11));
+
+        #Hidden Layer#2
+network.add(Dense(units=6,
+                  activation='relu',
+                  kernel_initializer='uniform'));
+
+        #Exit Layer
+network.add(Dense(units=1,
+                  activation='sigmoid',
+                  kernel_initializer='uniform'));
+
+from ann_visualizer import visualize
+
+visualize(network);
+```
+
+This will output:
+![photo](https://i.imgur.com/ngThGlk.png)
+
+## Contributions
+This library is still unstable. Please report all bug to the issues section. It is currently tested with `python3.5`, but it should run just fine on any python3.
---
+++ ann_visualizer.py
@@ -0,0 +1,75 @@
+def visualize(model, view=True, filename="network.gv"):
+    """Vizualizez a Sequential model.
+
+    # Arguments
+        model: A Keras model instance.
+
+        view: whether to display the model after generation.
+
+        filename: where to save the vizualization. (a .gv file)
+
+    """
+    from graphviz import Digraph;
+    import keras;
+    from keras import Sequential;
+    input_layer = 0;
+    hidden_layers_nr = 0;
+    hidden_layers = [];
+    output_layer = 0;
+    for layer in model.layers:
+        if(layer == model.layers[0]):
+            input_layer = int(str(layer.input_shape).split(",")[1][1:-1]);
+            hidden_layers_nr += 1;
+            hidden_layers.append(int(str(layer.output_shape).split(",")[1][1:-1]));
+        else:
+            if(layer == model.layers[-1]):
+                output_layer = int(str(layer.output_shape).split(",")[1][1:-1]);
+            else:
+                hidden_layers_nr += 1;
+                hidden_layers.append(int(str(layer.output_shape).split(",")[1][1:-1]));
+        last_layer_nodes = input_layer;
+        nodes_up = input_layer;
+
+        g = Digraph('g', filename=filename);
+        n = 0;
+        g.attr(splines="false", nodesep='1', ranksep='2');
+
+
+        with g.subgraph(name='cluster_input') as c:
+            c.attr(color='white')
+            for i in range(0, input_layer):
+                n += 1;
+                c.node(str(n));
+            c.attr(label='Input Layer')
+            c.attr(rank='same');
+            c.node_attr.update(color="#2ecc71", style="filled", fontcolor="#2ecc71", shape="circle");
+
+        for i in range(0, hidden_layers_nr):
+            with g.subgraph(name="cluster_"+str(i+1)) as c:
+                c.attr(color='white');
+                c.attr(rank='same');
+                for j in range(0, hidden_layers[i]):
+                    n += 1;
+                    c.node(str(n), shape="circle", style="filled", color="#3498db", fontcolor="#3498db");
+                    for h in range(nodes_up - last_layer_nodes + 1 , nodes_up + 1):
+                        g.edge(str(h), str(n));
+                last_layer_nodes = hidden_layers[i];
+                nodes_up += hidden_layers[i];
+
+
+        with g.subgraph(name='cluster_output') as c:
+            c.attr(color='white')
+            c.attr(rank='same');
+            c.attr(labeljust="1");
+            for i in range(1, output_layer+1):
+                n += 1;
+                c.node(str(n), shape="circle", style="filled", color="#e74c3c", fontcolor="#e74c3c");
+                for h in range(nodes_up - last_layer_nodes + 1 , nodes_up + 1):
+                    g.edge(str(h), str(n));
+            c.attr(label='Output Layer', labelloc="bottom")
+            c.node_attr.update(color="#2ecc71", style="filled", fontcolor="#2ecc71", shape="circle");
+
+        g.attr(arrowShape="none");
+        g.edge_attr.update(arrowhead="none", color="#707070");
+        if view == True:
+            g.view();
